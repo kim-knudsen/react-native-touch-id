75a50197a75fc49a737d2dae269b753b9e56dada
Kim Knudsen <kkn@magnetix.dk>
2019-06-07 14:42:10 +0200

    Avoid setting error messages to circumvent hard-coded error text

--- android/src/main/java/com/rnfingerprint/FingerprintDialog.java
+++ android/src/main/java/com/rnfingerprint/FingerprintDialog.java
@@ -172,7 +172,7 @@ public class FingerprintDialog extends DialogFragment implements FingerprintHand
 
     @Override
     public void onError(String errorString, int errorCode) {
-        this.mFingerprintError.setText(errorString);
+        // this.mFingerprintError.setText(errorString);
         this.mFingerprintImage.setColorFilter(this.imageErrorColor);
         this.mFingerprintSensorDescription.setText(this.sensorErrorDescription);
     }
